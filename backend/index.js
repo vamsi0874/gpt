4f2b9a82e1b62ae0402a5f058a6f12d9238cd010
vamsi0874 <vamsim0874>
2025-02-03 21:04:25 +0530

    ok

--- backend/index.js
+++ backend/index.js
@@ -39,12 +39,12 @@ const __dirname = path.dirname(__filename);
 //     credentials:true
 //   }
 // ));
-app.use(cors({
-  origin: process.env.CLIENT_URL,  // Replace with your actual frontend URL
-  credentials: true,
-}));
+// app.use(cors({
+//   origin: process.env.CLIENT_URL,  // Replace with your actual frontend URL
+//   credentials: true,
+// }));
 
-// app.use(cors())
+app.use(cors())
 
 
 
