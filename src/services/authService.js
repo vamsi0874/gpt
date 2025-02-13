cdc8c62e18d0eff0881440214922abbbee854113
vamsi0874 <vamsim0874>
2025-02-13 12:31:48 +0530

    login

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,3 +1,36 @@
+// import api from './api';
+
+// const authService = {
+//   login: async (email, password) => {
+
+//     try {
+   
+//     const response = await api.post('/users/login', { email, password });
+//     // console.log('responseeee',response)
+//     return response.data;
+//     }
+//     catch (err){
+ 
+//       console.error(err)
+//     }
+//   },
+//   signup: async (name, email, password) => {
+
+//     try {
+//     const response = await api.post('/users/signup', { name, email, password });
+//     // console.log('response',response)
+//     return response.data;
+//     }
+//     catch (err){
+//       // console.log(err.response.data.message)
+//       console.error(err.response.data.message)
+//     }
+//   },
+// };
+
+// export default authService;
+
+import axios from 'axios';
 import api from './api';
 
 const authService = {
@@ -5,7 +38,7 @@ const authService = {
 
     try {
    
-    const response = await api.post('/users/login', { email, password });
+    const response = await axios.post(`${import.meta.env.VITE_API_URL}/api/users/login`, { email, password });
     // console.log('responseeee',response)
     return response.data;
     }
@@ -29,3 +62,4 @@ const authService = {
 };
 
 export default authService;
+
